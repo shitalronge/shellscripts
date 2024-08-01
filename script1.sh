04a90771ac748ac367b59a946791d46d8bcca8fc
shitalronge <shitalronge94@gmail.com>
2024-08-01 12:27:30 +0530

    modified

--- script1.sh
+++ script1.sh
@@ -5,6 +5,6 @@ echo "We are learning git"
 echo "Husband: I don't want to travel"
 echo "Wife: I want to travel"
 echo "its my first line in github"
-echo "good morning to all"
+echo " good morning"
 
 
